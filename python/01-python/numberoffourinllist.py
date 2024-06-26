4b3f26b311b17dafce52ebb13a300289ceb4af2c
muhammed eldabea <muhammedeldabea@yahoo.com>
2024-06-26 21:36:28 +0300

    TASK for lesson 1

---
+++ python/01-python/numberoffourinllist.py
@@ -0,0 +1,11 @@
+fourlist = [1,2,4,4,5,4,6,6,7,4,8,9,4] 
+fourcounter = 0 
+
+for i in fourlist :
+    if i == 4 :
+        fourcounter +=1 
+    else : 
+        pass 
+
+print (fourlist.count(4) ) #print the result from the list dirct  
+print (fourcounter)  #print the resulkt based on C-style 
