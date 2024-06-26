1113d9e8ad688a09fe5ec041abab2241bcbc0470
muhammed eldabea <muhammedeldabea@yahoo.com>
2024-06-26 22:08:42 +0300

    TASK for lesson 1 add the os calenndar tasksk

---
+++ python/01-python/01-intro_to_python/calendara.py
@@ -0,0 +1,4 @@
+import calendar 
+yy = int( input ("input the year " )) 
+mm = int (input ("input the month" )) 
+print(calendar.month(yy, mm))
---
+++ python/01-python/01-intro_to_python/pathOSdefination.py
@@ -0,0 +1,5 @@
+import os 
+import platform 
+
+print (os.getcwd() ) 
+print (platform.system() ) 
