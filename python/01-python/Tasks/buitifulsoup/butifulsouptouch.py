e1b0dbc4c37785009fd260012d369b6e6bd770c4
muhammed-eldabea <muhammedeldabea@yahoo.com>
2024-06-30 19:52:39 +0300

    add butifulsoup examples

--- python/01-python/Tasks/buitifulsoup/butifulsouptouch.py
+++ python/01-python/Tasks/buitifulsoup/butifulsouptouch.py
@@ -1,7 +1,34 @@
-import bs4 as bs
+from bs4 import BeautifulSoup as  bs
 import time   as ts
 import requests as req 
 
 
-htmlconttent = req.get("http://http://localhost:5500/python/01-python/Tasks/buitifulsoup/").content 
-print(htmlconttent) 
+htmlconttent = req.get("http://localhost:5500/python/01-python/Tasks/buitifulsoup/index.html").content 
+#print(htmlconttent) 
+
+soup=bs(htmlconttent,"lxml") 
+# print(soup) 
+
+
+print("=============Paragraph=================")
+datacontent = soup.findAll("article" , {"class":"w3-container"} )
+
+
+# datacontent2 = soup.findAll("p","article" ) 
+
+
+for paragraph in datacontent:
+    print(paragraph.find("h2").text) 
+
+# print("=============Paragraph all =================")
+
+# for paragraph in datacontent2:
+#     print(paragraph.text) 
+
+
+# print(type(datacontent))
+# print("=============CityNames=================")
+# headrers = soup.find_all("h2") 
+
+# for heardh in headrers:
+#     print(heardh.text) 
